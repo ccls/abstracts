6a38300534101d6d44f68540759ae4d6ed25b9e6
Jake <github@jake.otherinbox.com>
2010-11-17 16:34:06 -0800

    Deving active resource for subject with homex app.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,9 +1,6 @@
 class Subject < ActiveResource::Base
-#	self.site = "https://apple:orange@ccls.berkeley.edu/homex"
+#	self.site = "https://ccls.berkeley.edu/homex/api"
 	self.site = "https://dev.sph.berkeley.edu/api"
-#	self.user = "admin"
-#	self.password = "secret"
-#	@headers = { 'calnetuid' => '859908' }
 
 	@@config = YAML::load(ERB.new(
 		IO.read('config/homex.yml')).result)
