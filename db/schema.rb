4798a7b4622b29a2e526c2077a38f408e8f00f68
Jake <github@jake.otherinbox.com>
2010-12-09 11:30:22 -0800

    Added simply_authorized gem to config/environment

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101115230636) do
+ActiveRecord::Schema.define(:version => 20101118214737) do
 
   create_table "abstracts", :force => true do |t|
     t.integer  "subject_id"
