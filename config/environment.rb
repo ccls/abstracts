c9eb30bbf0be92e49c9d2935acc481ef6d005948
Jake <github@jakewendt.com>
2011-01-19 14:59:06 -0800

    Moved rdoc_rails requirements into Rakefile.

--- config/environment.rb
+++ config/environment.rb
@@ -45,9 +45,6 @@ Rails::Initializer.run do |config|
 	config.gem 'jakewendt-simply_pages',
 		:lib    => 'simply_pages'
 
-#	config.gem 'jakewendt-rdoc_rails',
-#		:lib    => 'rdoc_rails'
-
 	config.gem "chronic"   #		http://chronic.rubyforge.org/
 	config.gem 'will_paginate'
 	config.gem 'fastercsv'
