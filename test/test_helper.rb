2a1c3e33318f5da560523eddc6377e792ad9feb8
Jake <github@jakewendt.com>
2011-05-26 14:09:40 -0700

    Moved abstracts into ccls engine.[#34]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,47 +7,47 @@ require 'partial_abstract_controller_test_helper'
 class ActiveSupport::TestCase
 	fixtures :all
 
-	def create_case_subject_with_patid(patid)
-		subject = create_subject( :subject_type => SubjectType['Case'] )
-		Factory(:identifier,
-			:subject => subject,
-			:patid   => patid )
-		subject
-	end
-
-	def create_abstracts(count=0,options={})
-		abstracts = []
-		count.times{ abstracts.push(create_abstract(options)) }
-		return abstracts
-	end
-
-	def create_abstract_with_first_name(first_name)
-		subject  = create_subject_with_first_name(first_name)
-		abstract = create_abstract
-		subject.abstracts << abstract
-		abstract
-	end
-
-	def create_abstract_with_last_name(last_name)
-		subject  = create_subject_with_last_name(last_name)
-		abstract = create_abstract
-		subject.abstracts << abstract
-		abstract
-	end
-
-	def create_abstract_with_childid(childid)
-		subject  = create_subject_with_childid(childid)
-		abstract = create_abstract
-		subject.abstracts << abstract
-		abstract
-	end
-
-	def create_abstract_with_patid(patid)
-		subject  = create_subject_with_patid(patid)
-		abstract = create_abstract
-		subject.abstracts << abstract
-		abstract
-	end
+#	def create_case_subject_with_patid(patid)
+#		subject = create_subject( :subject_type => SubjectType['Case'] )
+#		Factory(:identifier,
+#			:subject => subject,
+#			:patid   => patid )
+#		subject
+#	end
+#
+#	def create_abstracts(count=0,options={})
+#		abstracts = []
+#		count.times{ abstracts.push(create_abstract(options)) }
+#		return abstracts
+#	end
+#
+#	def create_abstract_with_first_name(first_name)
+#		subject  = create_subject_with_first_name(first_name)
+#		abstract = create_abstract
+#		subject.abstracts << abstract
+#		abstract
+#	end
+#
+#	def create_abstract_with_last_name(last_name)
+#		subject  = create_subject_with_last_name(last_name)
+#		abstract = create_abstract
+#		subject.abstracts << abstract
+#		abstract
+#	end
+#
+#	def create_abstract_with_childid(childid)
+#		subject  = create_subject_with_childid(childid)
+#		abstract = create_abstract
+#		subject.abstracts << abstract
+#		abstract
+#	end
+#
+#	def create_abstract_with_patid(patid)
+#		subject  = create_subject_with_patid(patid)
+#		abstract = create_abstract
+#		subject.abstracts << abstract
+#		abstract
+#	end
 
 end
 
