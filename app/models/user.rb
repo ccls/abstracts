c8aef6a2cb993998224b2ccfebc48ca78713cf32
Jake <github@jakewendt.com>
2011-09-13 11:36:45 -0700

    Renaming all things Subject to StudySubject.[#35]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < Ccls::User
 
-	%w(	abstracts bone_marrows checklists clinical_chemo_protocols cbcs cerebrospinal_fluids chest_imagings cytogenetics diagnoses discharges flow_cytometries histocompatibilities identifying_datas tdts therapy_responses names subjects ).each do |resource|
+	%w(	abstracts bone_marrows checklists clinical_chemo_protocols cbcs cerebrospinal_fluids chest_imagings cytogenetics diagnoses discharges flow_cytometries histocompatibilities identifying_datas tdts therapy_responses names study_subjects ).each do |resource|
 		alias_method "may_create_#{resource}?".to_sym,	:may_administrate?
 		alias_method "may_read_#{resource}?".to_sym,		:may_administrate?
 		alias_method "may_edit_#{resource}?".to_sym,		:may_administrate?
